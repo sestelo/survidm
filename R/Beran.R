a899f20c3e04e705b37ed1e460110bfc0a7d7065
Marta Sestelo <sestelo@uvigo.es>
2019-01-08 09:27:53 +0100

    hhh

--- R/Beran.R
+++ R/Beran.R
@@ -1,9 +1,21 @@
 Beran <-
   function(time, status, covariate, delta, x, y, kernel = "gaussian", bw,
            lower.tail = FALSE) {
+
+
+    if (missing(time)|is.null(time)) {
+      stop("Argument \"time\" is missing, with no default")
+    }
+    if (missing(status)|is.null(time)) {
+      stop("Argument \"status\" is missing, with no default")
+    }
+
+
     spa <- NULL
     len <- length(time)
     if (missing(delta)) delta <- rep(1, len)
+
+
     res <- .C( "SurvBeranKernel", as.double(time), as.integer(status),
                as.double(covariate), as.integer(delta), as.integer(len),
                as.double(y), as.double(x), as.double(bw),
